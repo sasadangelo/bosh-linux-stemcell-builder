19d1429472adb5a25f9861be51bbf5f636445ea3
Wang JianQiu <wangjq@cn.ibm.com>
2017-04-05 11:27:26 +0800

    Change bluemix stemcell name to light-bosh-stemcell-$CANDIDATE_BUILD_NUMBER-bluemix-xen-ubuntu-trusty-go_agent

--- ci/tasks/import-stemcell.sh
+++ ci/tasks/import-stemcell.sh
@@ -14,6 +14,6 @@ stemcell_vhd_filename="${stemcell_name}.vhd"
 echo -e "\n Softlayer create from external source..."
 IFS=':' read -ra OBJ_STORAGE_ACC_NAME <<< "$SWIFT_USERNAME"
 URI="swift://${OBJ_STORAGE_ACC_NAME}@${SWIFT_CLUSTER}/${SWIFT_CONTAINER}/${stemcell_vhd_filename}"
-$bosh_softlayer_tool -c import-image --os-ref-code UBUNTU_14_64 --uri ${URI} --public-name "light-bosh-stemcell-$CANDIDATE_BUILD_NUMBER-softlayer-xen-ubuntu-trusty-go_agent" \
+$bosh_softlayer_tool -c import-image --os-ref-code UBUNTU_14_64 --uri ${URI} --public-name "light-bosh-stemcell-$CANDIDATE_BUILD_NUMBER-bluemix-xen-ubuntu-trusty-go_agent" \
     --public-note "Public_light_stemcell_${CANDIDATE_BUILD_NUMBER}" --public | tail -1 >> "${base_gopath}/../stemcell-image/stemcell-info.json"
 
