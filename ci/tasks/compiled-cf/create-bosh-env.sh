ef99759d754e0d6e620da5ae531fd2684b0352b9
zhangtbj <zhangtbj@cn.ibm.com>
2017-05-18 14:26:23 +0800

    add x

--- ci/tasks/compiled-cf/create-bosh-env.sh
+++ ci/tasks/compiled-cf/create-bosh-env.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 set -ex
 
+source pipeline-src/ci/tasks/utils.sh
+
+check_param FILE_W3_BOSH_PEM
+
+echo "FILE_W3_BOSH_PEM: $FILE_W3_BOSH_PEM"
 touch director-state/director-state-1.txt
-echo "Hello Yes!" > director-state/director-state-1.txt
-mkdir -p bosh/publish/235018
+echo "Hello `date`!" > director-state/director-state-1.txt
+mkdir -p bosh/publish/235019
 echo $FILE_W3_BOSH_PEM > bosh/bosh.pem
-cp director-state/director-state-1.txt bosh/publish/235018/
+cp director-state/director-state-1.txt bosh/publish/235019/
+
 cd bosh
-scp -i ./bosh.pem -r publish/235018/director-state/director-state-1.txt bosh@file.w3.bluemix.net:~/repo
+scp -i bosh.pem -o "StrictHostKeyChecking no" -r publish/235019/director-state-1.txt bosh@file.w3.bluemix.net:~/repo
