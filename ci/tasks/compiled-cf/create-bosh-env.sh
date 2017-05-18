e3f9c06802adaa5ae00ac9a9f1a21fd186607229
zhangtbj <zhangtbj@cn.ibm.com>
2017-05-18 13:15:08 +0800

    add x

--- ci/tasks/compiled-cf/create-bosh-env.sh
+++ ci/tasks/compiled-cf/create-bosh-env.sh
@@ -1,72 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
-source pipeline-src/ci/tasks/utils.sh
-
-check_param SL_VM_PREFIX
-check_param SL_USERNAME
-check_param SL_API_KEY
-check_param SL_DATACENTER
-check_param SL_VLAN_PUBLIC
-check_param SL_VLAN_PRIVATE
-
-deployment_dir="${PWD}/deployment"
-mkdir -p $deployment_dir
-
-SL_VM_DOMAIN=${SL_VM_PREFIX}.softlayer.com
-
-STEMCELL_NAME="$(ls stemcell|grep tgz)"
-ORG_STEMCELL_NAME="light-bosh-stemcell-3312.9-softlayer-xen-ubuntu-trusty-go_agent.tgz"
-cp pipeline-src/ci/tasks/templates/bosh-template.yml bosh-template.yml
-sed -i 's/'"$ORG_STEMCELL_NAME"'/'"$STEMCELL_NAME"'/g' bosh-template.yml
-
-BOSH_CLI="$(pwd)/$(echo bosh-cli/bosh-cli-*)"
-chmod +x ${BOSH_CLI}
-
-  function finish {
-    echo "Final state of director deployment:"
-    echo "====================================================================="
-    cat bosh-template-state.json
-    echo "====================================================================="
-    echo "Director:"
-    echo "====================================================================="
-    cat /etc/hosts | grep "$SL_VM_DOMAIN" | tee ${deployment_dir}/director-hosts
-    echo "====================================================================="
-    echo "Saving config..."
-    cp $BOSH_CLI bosh-template.yml bosh-template-state.json ${deployment_dir}/
-    pushd ${deployment_dir}
-      tar -zcvf  /tmp/director-state-1.tgz ./ >/dev/null 2>&1
-    popd
-    mv /tmp/director-state-1.tgz director-state/
-  }
-
-trap finish ERR
-
-echo "Using bosh-cli $($BOSH_CLI -v)"
-echo "Deploying director..."
-
-$BOSH_CLI create-env bosh-template.yml \
-                      --state=bosh-template-state.json \
-                      --vars-store ${deployment_dir}/credentials.yml \
-                      -v SL_VM_PREFIX=${SL_VM_PREFIX} \
-                      -v SL_VM_DOMAIN=${SL_VM_DOMAIN} \
-                      -v SL_USERNAME=${SL_USERNAME} \
-                      -v SL_API_KEY=${SL_API_KEY} \
-                      -v SL_DATACENTER=${SL_DATACENTER} \
-                      -v SL_VLAN_PUBLIC=${SL_VLAN_PUBLIC} \
-                      -v SL_VLAN_PRIVATE=${SL_VLAN_PRIVATE}
-
-echo "Trying to set target to director..."
-
-$BOSH_CLI  -e ${SL_VM_DOMAIN} --ca-cert <($BOSH_CLI int ${deployment_dir}/credentials.yml --path /DIRECTOR_SSL/ca) alias-env bosh-env
-
-echo "Trying to login to director..."
-
-export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=$(${BOSH_CLI} int ${deployment_dir}/credentials.yml --path /DI_ADMIN_PASSWORD)
-
-$BOSH_CLI -e bosh-env login
-
-trap - ERR
-
-finish
+touch director-state/director-state-1.txt
+echo "Hello Yes!" > director-state/director-state-1.txt
+mkdir -p bosh/publish/235018
+echo $FILE_W3_BOSH_PEM > bosh/bosh.pem
+cp director-state/director-state-1.txt bosh/publish/235018/
+cd bosh
+scp -i ./bosh.pem -r publish/235018/director-state/director-state-1.txt bosh@file.w3.bluemix.net:~/repo
