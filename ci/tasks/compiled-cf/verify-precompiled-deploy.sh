8575d2480e1adb65bd3fbc881b1c1473a33025e9
zhangtbj <zhangtbj@cn.ibm.com>
2017-05-19 22:09:33 +0800

    add verify job

---
+++ ci/tasks/compiled-cf/verify-precompiled-deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eux
+
+source pipeline-src/ci/tasks/utils.sh
+
+check_param BUILD_VERSION
+check_param SL_VM_PREFIX
+check_param SL_USERNAME
+check_param SL_API_KEY
+check_param SL_DATACENTER
+check_param SL_VLAN_PUBLIC
+check_param SL_VLAN_PRIVATE
+
+cd compiled-release
+tar -xvf cf-compiled-release-${BUILD_VERSION}.tgz
