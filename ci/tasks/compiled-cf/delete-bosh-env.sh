1fa512b9b3c7b94c7dba68661044f491613a864b
zhangtbj <zhangtbj@cn.ibm.com>
2017-05-19 15:01:46 +0800

    fix pipeline issue

--- ci/tasks/compiled-cf/delete-bosh-env.sh
+++ ci/tasks/compiled-cf/delete-bosh-env.sh
@@ -4,6 +4,7 @@ set -eux
 
 source pipeline-src/ci/tasks/utils.sh
 
+check_param BUILD_VERSION
 check_param SL_VM_PREFIX
 check_param SL_USERNAME
 check_param SL_API_KEY
@@ -15,7 +16,7 @@ check_param SL_VLAN_PRIVATE
 # target/authenticate
 #
 
-tar -zxvf director-state/director-state.tgz -C director-state/
+tar -zxvf director-state/director-state-${BUILD_VERSION}.tgz -C director-state/
 cat director-state/director-hosts >> /etc/hosts
 
 BOSH_CLI="$(pwd)/$(echo bosh-cli/bosh-cli-*)"
