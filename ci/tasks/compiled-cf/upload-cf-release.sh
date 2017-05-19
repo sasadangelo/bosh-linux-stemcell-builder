594b147b15571bd80b4ca3e3f35bfbb8a85966ec
zhangtbj <zhangtbj@cn.ibm.com>
2017-05-19 18:44:55 +0800

    fix upload issue

--- ci/tasks/compiled-cf/upload-cf-release.sh
+++ ci/tasks/compiled-cf/upload-cf-release.sh
@@ -8,10 +8,14 @@ check_param cf_release
 check_param BUILD_VERSION
 check_param FILE_W3_BOSH_PEM
 
+pwd
 mkdir -p bosh/publish/${cf_release}
-echo "FILE_W3_BOSH_PEM: ${FILE_W3_BOSH_PEM}"
+#echo "FILE_W3_BOSH_PEM: ${FILE_W3_BOSH_PEM}"
 echo ${FILE_W3_BOSH_PEM} > bosh/bosh.pem
-cp cf-compiled-release-${BUILD_VERSION}.tgz bosh/publish/${cf_release}/
+mv compiled-release/cf-compiled-release-${BUILD_VERSION}.tgz bosh/publish/${cf_release}/
 
 cd bosh
 scp -i bosh.pem -o "StrictHostKeyChecking no" -r publish/${cf_release}/cf-compiled-release-${BUILD_VERSION}.tgz bosh@file.w3.bluemix.net:~/repo
+
+echo "scp cf-compiled-release-${BUILD_VERSION}.tgz file to file w3:"
+ehco "http://file.w3.bluemix.net/releases/bosh/${cf_release}/cf-compiled-release-${BUILD_VERSION}.tgz"
