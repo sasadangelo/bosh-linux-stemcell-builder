9d67e6341165a76bd18defff767e910c2d9dd9a7
zhangtbj <zhangtbj@cn.ibm.com>
2017-05-22 21:53:56 +0800

    test new export

--- ci/tasks/compiled-cf/upload-cf-release.sh
+++ ci/tasks/compiled-cf/upload-cf-release.sh
@@ -5,17 +5,15 @@ set -eux
 source pipeline-src/ci/tasks/utils.sh
 
 check_param BUILD_VERSION
-check_param cf_release
-check_param cf_release_version
 check_param FILE_W3_BOSH_PEM
 
 STEMCELL_VERSION=$(cat stemcell/version)
 
 pwd
-mkdir -p bosh/publish/${cf_release_version}
+mkdir -p bosh/publish/precompiled
 #echo "FILE_W3_BOSH_PEM: ${FILE_W3_BOSH_PEM}"
 echo ${FILE_W3_BOSH_PEM} > bosh/bosh.pem
-mv compiled-release/*-${BUILD_VERSION}.tgz bosh/publish/${cf_release_version}/
+mv compiled-release/compiled-release-allinone-${BUILD_VERSION}.tgz bosh/publish/precompiled/
 
 cd bosh
 sed -i "s/-----BEGIN RSA PRIVATE KEY----- //g" bosh.pem
@@ -25,7 +23,7 @@ sed -i "1i\-----BEGIN RSA PRIVATE KEY-----" bosh.pem
 sed -i "\$a-----END RSA PRIVATE KEY-----" bosh.pem
 cat bosh.pem
 chmod 400 bosh.pem
-scp -i bosh.pem -o "StrictHostKeyChecking no" -r publish/${cf_release_version}/ bosh@file.w3.bluemix.net:~/repo
+scp -i bosh.pem -o "StrictHostKeyChecking no" -r publish/precompiled/ bosh@file.w3.bluemix.net:~/repo
 
-echo "scp ${cf_release}-${cf_release_version}-ubuntu-trusty-${STEMCELL_VERSION}-${BUILD_VERSION}.tgz file to file w3:"
-echo "http://file.w3.bluemix.net/releases/bosh/${cf_release_version}/${cf_release}-${cf_release_version}-ubuntu-trusty-${STEMCELL_VERSION}-${BUILD_VERSION}.tgz"
+echo "scp compiled-release-allinone-${BUILD_VERSION}.tgz file to file w3:"
+echo "http://file.w3.bluemix.net/releases/bosh/precompiled/compiled-release-allinone-${BUILD_VERSION}.tgz"
