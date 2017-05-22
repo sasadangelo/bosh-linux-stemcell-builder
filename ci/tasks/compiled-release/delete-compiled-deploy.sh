dc81b404035676310a1ad1fe699077de0c2e957d
zhangtbj <zhangtbj@cn.ibm.com>
2017-05-23 06:49:46 +0800

    fix issue on delete job

--- ci/tasks/compiled-release/delete-compiled-deploy.sh
+++ ci/tasks/compiled-release/delete-compiled-deploy.sh
@@ -31,7 +31,7 @@ $BOSH_CLI -e bosh-env deployments
 # release_string=`cat compiled-deploy/compiled-deploy-${BUILD_VERSION}.yml | grep -Po '(?<=- location: ).*' | sed "s/.*releases\///g" | sed "s/\/.*//g"`
 # Check which release is installed directly
 release_string=`$BOSH_CLI -e bosh-env releases | awk -F ' ' '{print $1}'`
-IFS=', ' read -r -a release_array <<< "$release_string"
+release_array=($release_string)
 
 for release in "${release_array[@]}"
 do
