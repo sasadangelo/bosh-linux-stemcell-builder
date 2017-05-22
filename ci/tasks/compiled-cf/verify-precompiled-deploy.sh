cdaa9aa9dcd779a8243f82be7d4ae968c3d2ee99
zhangtbj <zhangtbj@cn.ibm.com>
2017-05-22 15:35:33 +0800

    refine verify job

--- ci/tasks/compiled-cf/verify-precompiled-deploy.sh
+++ ci/tasks/compiled-cf/verify-precompiled-deploy.sh
@@ -13,9 +13,13 @@ check_param SL_VLAN_PUBLIC
 check_param SL_VLAN_PRIVATE
 SL_VM_PREFIX=${SL_VM_PREFIX}-${BUILD_VERSION}
 
+STEMCELL_VERSION=$(cat stemcell/version)
 
 tar -zxvf director-state/director-state-${BUILD_VERSION}.tgz -C director-state/
 cat director-state/director-hosts >> /etc/hosts
+
+mv compiled-release/cf-compiled-release-${BUILD_VERSION}.tgz compiled-release/${cf_release}-${cf_release_version}-ubuntu-trusty-${STEMCELL_VERSION}-${BUILD_VERSION}.tgz
+
 
 BOSH_CLI="$(pwd)/$(echo bosh-cli/bosh-cli-*)"
 chmod +x ${BOSH_CLI}
@@ -31,5 +35,4 @@ export BOSH_CLIENT_SECRET=$(${BOSH_CLI} int director-state/credentials.yml --pat
 
 $BOSH_CLI -e bosh-env login
 
-$BOSH_CLI -e bosh-env releases
-
+$BOSH_CLI -e bosh-env upload-release compiled-release/${cf_release}-${cf_release_version}-ubuntu-trusty-${STEMCELL_VERSION}-${BUILD_VERSION}.tgz
