762ebc68fd7dc01409dd23a68b1f1cdbd7f6f473
Matt Cui <mattcui@gmail.com>
2017-05-25 17:12:20 +0800

    Change to use s3 resource to copy file

--- ci/tasks/publish.sh
+++ ci/tasks/publish.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
-set -u
+set -e
 
-export VERSION=$( cat version/number | sed 's/\.0$//;s/\.0$//' )
-stemcell=$(realpath stemcell/*.tgz)
-
-for file in $COPY_KEYS ; do
-  file="${file/\%s/$VERSION}"
-
-  echo "$file"
-  echo "$stemcell"
-
-  checksum="$(sha1sum "${stemcell}" | awk '{print $1}')"
-  echo "$file sha1=$checksum"
-
-  # occasionally this fails for unexpected reasons; retry a few times
-  for i in {1..4}; do
-    aws --endpoint-url "https://${AWS_ENDPOINT}" s3 cp --acl public-read "s3://${CANDIDATE_BUCKET_NAME}/$file" "s3://${PUBLISHED_BUCKET_NAME}/$file" \
-      && break \
-      || sleep 5
-  done
-
-done
+mkdir -p light-bluemix-stemcell-prod
+cp light-bluemix-stemcell/*.tgz light-bluemix-stemcell-prod/
 
 fileUrl=https://s3-api.us-geo.objectstorage.softlayer.net/${PUBLISHED_BUCKET_NAME}/${file}
 echo -e "Stemcell Download URL -> ${fileUrl}"
