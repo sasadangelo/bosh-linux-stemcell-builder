886554e0cdbbe4cb630e11864d330ddca359f17a
Wang JianQiu <wangjq@cn.ibm.com>
2017-05-29 13:47:29 +0800

    fixed unit test failure

--- ci/tasks/deploy-director-softlayer.sh
+++ ci/tasks/deploy-director-softlayer.sh
@@ -51,7 +51,7 @@ $bosh_cli create-env director.yml -l director-creds.yml
 # before nginx is reachable causing "Cannot talk to director..." messages.
 sleep 10
 
-export BOSH_ENVIRONMENT=`$bosh_cli int director-creds.yml --path /internal_ip`
+export BOSH_ENVIRONMENT=$BOSH_internal_ip
 export BOSH_CA_CERT=`$bosh_cli int director-creds.yml --path /director_ssl/ca`
 export BOSH_CLIENT="admin"
 export BOSH_CLIENT_SECRET=`$bosh_cli int director-creds.yml --path /admin_password`
