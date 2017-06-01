6cf45ca7a6df12d8778345a1cc79e0bcb2e47e62
Wang JianQiu <wangjq@cn.ibm.com>
2017-06-01 17:15:26 +0800

    skip ipv6 testcases

--- ci/tasks/deploy-director-softlayer.sh
+++ ci/tasks/deploy-director-softlayer.sh
@@ -51,7 +51,7 @@ $bosh_cli create-env director.yml -l director-creds.yml
 # before nginx is reachable causing "Cannot talk to director..." messages.
 sleep 10
 
-export BOSH_ENVIRONMENT=$BOSH_internal_ip
+export BOSH_ENVIRONMENT=`$bosh_cli int director-creds.yml --path /internal_ip`
 export BOSH_CA_CERT=`$bosh_cli int director-creds.yml --path /director_ssl/ca`
 export BOSH_CLIENT="admin"
 export BOSH_CLIENT_SECRET=`$bosh_cli int director-creds.yml --path /admin_password`
@@ -65,6 +65,3 @@ $bosh_cli -n update-cloud-config bosh-deployment/softlayer/cloud-config.yml \
           -v sl_vlan_public_id=$SL_VLAN_PUBLIC \
           -v sl_vlan_private_id=$SL_VLAN_PRIVATE \
           --vars-env "BOSH"
-
-mv $HOME/.bosh director-state/
-mv director.yml director-creds.yml director-state.json director-state/
