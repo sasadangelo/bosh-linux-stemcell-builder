c76449f08ee7d0f02cd12e60c822d521a3ccacb6
zhangtbj <zhangtbj@cn.ibm.com>
2017-08-02 16:03:18 +0800

    update pipeline add  bosh-template.yml

--- ci/tasks/compiled-release/create-bosh-env.sh
+++ ci/tasks/compiled-release/create-bosh-env.sh
@@ -18,6 +18,7 @@ SL_VM_PREFIX=${SL_VM_PREFIX}-${BUILD_VERSION}
 deployment_dir="${PWD}/deployment"
 mkdir -p $deployment_dir
 cp pipeline-src/ci/tasks/templates/bosh-template.yml bosh-template.yml
+SL_VM_DOMAIN=${SL_VM_PREFIX}.softlayer.com
 
 mkdir bosh-release
 wget ${DIRECTOR_URL} -P bosh-release
