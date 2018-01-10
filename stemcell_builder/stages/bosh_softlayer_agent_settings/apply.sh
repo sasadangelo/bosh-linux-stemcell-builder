5245c86abd519855da5c50c2bf4840c9e1fb30b7
Zida <shawzida@gmail.com>
2018-01-10 20:14:59 +0800

    revert agent_settings

--- stemcell_builder/stages/bosh_softlayer_agent_settings/apply.sh
+++ stemcell_builder/stages/bosh_softlayer_agent_settings/apply.sh
@@ -3,24 +3,21 @@
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-# Set SettingsPath but never use it because file_meta_service is avaliable only when the settings file exists.
+# Set SettingsPath but never use it because file_meta_service is available only when the settings file exists.
 cat > $chroot/var/vcap/bosh/agent.json <<JSON
 {
   "Platform": {
     "Linux": {
       "CreatePartitionIfNoEphemeralDisk": true,
-      "ScrubEphemeralDisk": true,
-      "DevicePathResolutionType": "iscsi"
+      "ScrubEphemeralDisk": true
     }
   },
   "Infrastructure": {
     "Settings": {
       "Sources": [
         {
-          "Type": "HTTP",
-          "URI": "https://api.service.softlayer.com",
-          "UserDataPath": "/rest/v3.1/SoftLayer_Resource_Metadata/getUserMetadata.json",
-          "InstanceIDPath": "/rest/v3.1/SoftLayer_Resource_Metadata/getId.json"
+          "Type": "File",
+          "SettingsPath": "/var/vcap/bosh/user_data.json"
         }
       ],
       "UseRegistry": true
