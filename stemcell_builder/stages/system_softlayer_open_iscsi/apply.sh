a43ec85a1d59199cac85f566fe3e9a46a08848b7
Zida <shawzida@gmail.com>
2018-05-21 16:32:02 +0800

    append iscsid restart for xenial

--- stemcell_builder/stages/system_softlayer_open_iscsi/apply.sh
+++ stemcell_builder/stages/system_softlayer_open_iscsi/apply.sh
@@ -8,4 +8,17 @@ source $base_dir/lib/prelude_apply.bash
 
 pkg_mgr install open-iscsi
 
-
+# append 'service iscsid restart' in /etc/init.d/open-iscsi stop
+if [ -f $chroot/etc/debian_version ] # Ubuntu
+then
+  if [ ${DISTRIB_CODENAME} == 'xenial' ]; then
+    if [ -f $chroot/etc/init.d/open-iscsi ]
+    then
+      sed -i '
+/^restart() {$/ {
+n
+a\\tservice iscsid restart
+}' $chroot/etc/init.d/open-iscsi
+    fi
+  fi
+fi
