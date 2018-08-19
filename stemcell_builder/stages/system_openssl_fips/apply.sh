206f043f2c6a0d85aebfa46df62d5e221f00077b
Zida <shawzida@gmail.com>
2018-08-19 23:13:16 +0800

    Add softlayer assets for xenial

---
+++ stemcell_builder/stages/system_openssl_fips/apply.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+base_dir=$(readlink -nf $(dirname $0)/../..)
+source $base_dir/lib/prelude_apply.bash
+source $base_dir/lib/prelude_bosh.bash
+
+cp -a $assets_dir/install.sh $chroot/tmp
+run_in_chroot $chroot "
+chmod +x /tmp/install.sh
+/tmp/install.sh
+rm -f /tmp/install.sh
+"
